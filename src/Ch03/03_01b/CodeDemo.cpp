633bf8f7c3e1b353b0be52ce5d40ef8ff0832cc2
amrutha-am <amruthajagadeesan@gmail.com>
2024-09-03 06:28:40 +0000

    my code on structure

--- src/Ch03/03_01b/CodeDemo.cpp
+++ src/Ch03/03_01b/CodeDemo.cpp
@@ -5,8 +5,20 @@
 #include <iostream>
 #include <string>
 
+enum class cow_purpose {diary, meat, pe, hide};
+
+struct cow{
+    std::string cow_name;
+    int age;
+    cow_purpose purpose;
+};
+
 int main(){
-    
-    std::cout << std::endl << std::endl;
+    cow my_cow = {"Betsy", 5, cow_purpose::diary};
+   /* my_cow.cow_name = "Betsy";
+    my_cow.age = 5;
+    my_cow.purpose = cow_purpose::diary;*/
+    std::cout << my_cow.cow_name <<" is a type "<<(int)my_cow.purpose <<" cow."<< std::endl;
+    std::cout << "Age of "<< my_cow.cow_name<<" is "<<my_cow.age<<"."<< std::endl << std::endl;
     return (0);
 }
